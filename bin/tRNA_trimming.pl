0dca2d9d7fc894e1c17c8f3c132972da59bfe158
Matthew Berg <mberg2@uwo.ca>
2019-05-28 11:06:11 -0400

    Updated license

--- bin/tRNA_trimming.pl
+++ bin/tRNA_trimming.pl
@@ -10,6 +10,18 @@ use warnings;
 #To run enter tRNA_trimming.pl tRNA_variant_sequence.fasta
 #tRNA_variant_sequence.fasta comes from BLAST_analysis.pl and there is one file for every sample (run with tRNA_trimming_run.sh to analyze and merge all files)
 
+# Copyright (C) 2019 Matthew Berg
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
 #####Reads in input files
 ##############################################################################
 
